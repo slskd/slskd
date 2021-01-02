07dfd25f47fd36809c5fa9af835122b86a34c291
JP Dillingham <jpdillingham@gmail.com>
2021-01-02 13:40:34 -0600

    stub tests

---
+++ tests/slskd.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace slskd.Tests.Integration
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
