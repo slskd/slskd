f5071fce7461949be7ffac129f283815a3ecd389
JP Dillingham <jpdillingham@gmail.com>
2021-02-02 21:28:39 -0600

    add Validation namespace and components

---
+++ src/slskd/Common/Validation/ValidateAttribute.cs
@@ -0,0 +1,26 @@
+﻿namespace slskd.Validation
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ValidateAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var results = new List<ValidationResult>();
+            var context = new ValidationContext(value, null, null);
+
+            Validator.TryValidateObject(value, context, results, true);
+
+            if (results.Count != 0)
+            {
+                var compositeResults = new CompositeValidationResult(validationContext.DisplayName);
+                results.ForEach(compositeResults.AddResult);
+
+                return compositeResults;
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
