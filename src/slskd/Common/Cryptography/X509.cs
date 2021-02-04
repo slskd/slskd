f562cfc7b8a8f2aaad72729a8f1df6578b32821f
JP Dillingham <jpdillingham@gmail.com>
2021-02-04 17:59:50 -0600

    add X509

---
+++ src/slskd/Common/Cryptography/X509.cs
@@ -0,0 +1,26 @@
+﻿namespace slskd.Common.Cryptography
+{
+    using System;
+    using System.Security.Cryptography;
+    using System.Security.Cryptography.X509Certificates;
+
+    public class X509
+    {
+        public static X509Certificate2 Generate(string subject, string password, X509KeyStorageFlags x509KeyStorageFlags)
+        {
+            using RSA rsa = RSA.Create(2048);
+
+            var request = new CertificateRequest(
+                new X500DistinguishedName($"CN={subject}"), 
+                rsa, 
+                HashAlgorithmName.SHA256, 
+                RSASignaturePadding.Pkcs1);
+
+            var certificate = request.CreateSelfSigned(
+                new DateTimeOffset(DateTime.UtcNow.AddDays(-1)), 
+                new DateTimeOffset(DateTime.UtcNow.AddDays(36500)));
+
+            return new X509Certificate2(certificate.Export(X509ContentType.Pkcs12, password), password, x509KeyStorageFlags);
+        }
+    }
+}
