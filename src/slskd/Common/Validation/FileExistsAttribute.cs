32baf3b17b7c07ad3be8756ae5193096f2be54a7
JP Dillingham <jpdillingham@gmail.com>
2021-02-06 14:09:13 -0600

    add FileExists validator

---
+++ src/slskd/Common/Validation/FileExistsAttribute.cs
@@ -0,0 +1,20 @@
+﻿namespace slskd.Validation
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.IO;
+
+    public class FileExistsAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var file = Path.GetFullPath(value.ToString());
+
+            if (!string.IsNullOrEmpty(file) && !File.Exists(file))
+            {
+                return new ValidationResult($"The {validationContext.DisplayName} field specifies a non-existent file '{file}'.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
