61c8202ab07d8cec93d0ba03e2910fa823aa2bde
JP Dillingham <jpdillingham@gmail.com>
2021-02-09 08:46:34 -0600

    add header

--- src/slskd/Common/Cryptography/X509.cs
+++ src/slskd/Common/Cryptography/X509.cs
@@ -1,10 +1,27 @@
-﻿namespace slskd.Common.Cryptography
+﻿// <copyright file="X509.cs" company="slskd Team">
+//     Copyright (c) slskd Team. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU Affero General Public License as published
+//     by the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU Affero General Public License for more details.
+//
+//     You should have received a copy of the GNU Affero General Public License
+//     along with this program.  If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace slskd.Common.Cryptography
 {
     using System;
     using System.Security.Cryptography;
     using System.Security.Cryptography.X509Certificates;
 
-    public class X509
+    public static class X509
     {
         public static X509Certificate2 Generate(string subject, string password = null, X509KeyStorageFlags x509KeyStorageFlags = X509KeyStorageFlags.MachineKeySet)
         {
