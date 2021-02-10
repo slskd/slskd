0ad3a9b5b584697b43b74f68e88a31e048b32822
JP Dillingham <jpdillingham@gmail.com>
2021-02-10 08:27:10 -0600

    docs

--- src/slskd/Common/Cryptography/X509.cs
+++ src/slskd/Common/Cryptography/X509.cs
@@ -21,8 +21,18 @@ namespace slskd.Common.Cryptography
     using System.Security.Cryptography;
     using System.Security.Cryptography.X509Certificates;
 
+    /// <summary>
+    ///     X509 utility methods.
+    /// </summary>
     public static class X509
     {
+        /// <summary>
+        ///     Generates an X509 certificate for the specified <paramref name="subject"/>.
+        /// </summary>
+        /// <param name="subject">The certificate subject.</param>
+        /// <param name="password">The optional password for the certificate.</param>
+        /// <param name="x509KeyStorageFlags">The optional key storage flags for the certificate.</param>
+        /// <returns>The generated certificate.</returns>
         public static X509Certificate2 Generate(string subject, string password = null, X509KeyStorageFlags x509KeyStorageFlags = X509KeyStorageFlags.MachineKeySet)
         {
             password ??= Guid.NewGuid().ToString();
@@ -42,6 +52,13 @@ namespace slskd.Common.Cryptography
             return new X509Certificate2(certificate.Export(X509ContentType.Pkcs12, password), password, x509KeyStorageFlags);
         }
 
+        /// <summary>
+        ///     Validates an X509 certificate given a PFX <paramref name="fileName"/> and optional <paramref name="password"/>.
+        /// </summary>
+        /// <param name="fileName">The path to the PFX certificate file to validate.</param>
+        /// <param name="password">The optional password for the certificate.</param>
+        /// <param name="result">The error message, if the certificate is invalid.</param>
+        /// <returns>A value indicating whether the certificate is valid.</returns>
         public static bool TryValidate(string fileName, string password, out string result)
         {
             result = null;
