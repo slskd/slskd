cb11b625b7ad9fa9d1d2221cdda0b97476c2fd9c
JP Dillingham <jpdillingham@gmail.com>
2021-05-20 07:36:36 -0500

    remove log

--- src/slskd/Common/Validation/X509CertificateAttribute.cs
+++ src/slskd/Common/Validation/X509CertificateAttribute.cs
@@ -1,9 +1,29 @@
-﻿namespace slskd.Validation
+﻿// <copyright file="X509CertificateAttribute.cs" company="slskd Team">
+//     Copyright (c) slskd Team. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU Affero General Public License as published
+//     by the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU Affero General Public License for more details.
+//
+//     You should have received a copy of the GNU Affero General Public License
+//     along with this program.  If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace slskd.Validation
 {
     using System.ComponentModel.DataAnnotations;
     using slskd.Common.Cryptography;
     using static slskd.Options.WebOptions.HttpsOptions;
 
+    /// <summary>
+    ///     Validates X509 certificate parameters specified through <see cref="CertificateOptions"/>.
+    /// </summary>
     public class X509CertificateAttribute : ValidationAttribute
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
@@ -11,8 +31,6 @@
             if (value != null)
             {
                 var cert = (CertificateOptions)value;
-
-                System.Console.WriteLine($"pass: {cert.Password} {string.IsNullOrEmpty(cert.Password)}");
 
                 if (!string.IsNullOrEmpty(cert.Pfx) && !X509.TryValidate(cert.Pfx, cert.Password, out var certResult))
                 {
