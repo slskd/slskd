675fbc147bdb17a51218392696a0fd7065e7ab17
JP Dillingham <jpdillingham@gmail.com>
2021-05-19 22:14:45 -0500

    add X509CertificateAttribute

---
+++ src/slskd/Common/Validation/X509CertificateAttribute.cs
@@ -0,0 +1,26 @@
+﻿namespace slskd.Validation
+{
+    using System.ComponentModel.DataAnnotations;
+    using slskd.Common.Cryptography;
+    using static slskd.Options.WebOptions.HttpsOptions;
+
+    public class X509CertificateAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                var cert = (CertificateOptions)value;
+
+                System.Console.WriteLine($"pass: {cert.Password} {string.IsNullOrEmpty(cert.Password)}");
+
+                if (!string.IsNullOrEmpty(cert.Pfx) && !X509.TryValidate(cert.Pfx, cert.Password, out var certResult))
+                {
+                    return new ValidationResult($"Invalid HTTPs certificate: {certResult}");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
