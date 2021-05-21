347a369779cb51f6f514ea8c8c865268865d6b81
JP Dillingham <jpdillingham@gmail.com>
2021-05-20 22:01:42 -0500

    more warnings

--- src/slskd/Common/Validation/CompositeValidationResult.cs
+++ src/slskd/Common/Validation/CompositeValidationResult.cs
@@ -1,8 +1,28 @@
-﻿namespace slskd.Validation
-{ 
+﻿// <copyright file="CompositeValidationResult.cs" company="slskd Team">
+//     Copyright (c) slskd Team. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU Affero General Public License as published
+//     by the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU Affero General Public License for more details.
+//
+//     You should have received a copy of the GNU Affero General Public License
+//     along with this program.  If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace slskd.Validation
+{
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
 
+    /// <summary>
+    ///     The composite result of a recursive validation.
+    /// </summary>
     public class CompositeValidationResult : ValidationResult
     {
         public CompositeValidationResult(string errorMessage)
--- src/slskd/Common/Validation/ValidationExtensions.cs
+++ src/slskd/Common/Validation/ValidationExtensions.cs
@@ -1,22 +1,33 @@
-﻿namespace slskd.Validation
+﻿// <copyright file="ValidationExtensions.cs" company="slskd Team">
+//     Copyright (c) slskd Team. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU Affero General Public License as published
+//     by the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU Affero General Public License for more details.
+//
+//     You should have received a copy of the GNU Affero General Public License
+//     along with this program.  If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace slskd.Validation
 {
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.Linq;
 
+    /// <summary>
+    ///     Extension methods for types involved with validaiton.
+    /// </summary>
     public static class ValidationExtensions
     {
-        public static string GetResultView(this ValidationResult result, string message)
-        {
-            var view = result.GetResultView(0).ToList();
-
-            if (view.Count > 0 && !string.IsNullOrEmpty(message))
-            {
-                view[0] = message;
-            }
-
-            return string.Join("\n", view);
-        }
+        public static string GetResultView(this CompositeValidationResult result)
+            => string.Join("\n", result.GetResultView(0));
 
         private static IEnumerable<string> GetResultView(this ValidationResult result, int depth = 0)
         {
