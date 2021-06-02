aeb48015536ee347b91798a637c8e993a4889fd2
JP Dillingham <jpdillingham@gmail.com>
2021-06-02 09:27:41 -0500

    add image placeholder, basic stats

--- src/web/src/components/Users/User.js
+++ src/web/src/components/Users/User.js
@@ -1,7 +1,12 @@
 import { 
   Item,
-  Image
+  Icon,
+  Divider,
+  Button
 } from 'semantic-ui-react';
+
+const ImagePlaceholder = () => 
+  (<div className='users-picture-placeholder ui small image'><Icon name='camera' size='big'/></div>);
 
 const User = ({ 
     username, 
@@ -14,16 +19,33 @@ const User = ({
     updatedAt,
     fetching
   }) => (
-    <Item>
-      <Item.Image size='small' src={`data:image;base64,${picture}`} />
+    <>
+      <Item>
+        {hasPicture ? 
+          <Item.Image size='small' src={`data:image;base64,${picture}`} />
+          : <ImagePlaceholder/>}
 
-      <Item.Content>
-        <Item.Header as='a'>{username}</Item.Header>
-        <Item.Description>
-          {description}
-        </Item.Description>
-      </Item.Content>
-    </Item>
+        <Item.Content>
+          <Item.Header as='a'>{username}</Item.Header>
+          <Item.Meta>
+          Free Upload Slot: <Icon 
+              name={hasFreeUploadSlot ? 'check' : 'close'}
+              color={hasFreeUploadSlot ? 'green' : 'red'}
+            />, Total Upload Slots: {uploadSlots}, Queue Length: {queueLength}
+          </Item.Meta>
+          <Item.Description>
+            {description || 'No user info.'}
+          </Item.Description>
+          <div className='users-statistics'>
+
+      </div>
+        </Item.Content>
+      </Item>
+      <Divider/>
+      <div>
+        <Button>Force Refresh</Button>
+      </div>
+    </>
 );
 
 export default User;
