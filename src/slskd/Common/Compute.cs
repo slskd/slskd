9704a8eb12576494a283b5c44ffe4a42b29653aa
JP Dillingham <jpdillingham@gmail.com>
2021-06-11 16:03:46 -0500

    simplify

--- src/slskd/Common/Compute.cs
+++ src/slskd/Common/Compute.cs
@@ -29,8 +29,7 @@ namespace slskd
             iteration = Math.Min(100, iteration);
 
             var computedDelay = Math.Floor((Math.Pow(2, iteration) - 1) / 2) * 1000;
-            var safeDelay = (int)Math.Min(computedDelay, int.MaxValue);
-            var clampedDelay = Math.Min(safeDelay, maxDelayInMilliseconds);
+            var clampedDelay = (int)Math.Min(computedDelay, maxDelayInMilliseconds);
 
             var jitter = new Random().Next(1000);
 
