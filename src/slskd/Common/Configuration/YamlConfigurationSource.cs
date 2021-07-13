7cb3260349e0affb271d0a44cc78c4f9139a8b94
JP Dillingham <jpdillingham@gmail.com>
2021-07-12 20:40:54 -0500

    rename back to Common

--- src/slskd/Common/Configuration/YamlConfigurationSource.cs
+++ src/slskd/Common/Configuration/YamlConfigurationSource.cs
@@ -15,7 +15,7 @@
 //     along with this program.  If not, see https://www.gnu.org/licenses/.
 // </copyright>
 
-namespace slskd.ConfigurationSources
+namespace slskd.Configuration
 {
     using System;
     using System.IO;
