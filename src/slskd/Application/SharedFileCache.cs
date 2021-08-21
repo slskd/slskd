05c9d7308aa2ef405352271f4254a519c397d317
JP Dillingham <jpdillingham@gmail.com>
2021-08-21 12:05:35 -0500

    exclude terms prefixed with -

--- src/slskd/Application/SharedFileCache.cs
+++ src/slskd/Application/SharedFileCache.cs
@@ -275,17 +275,16 @@ namespace slskd
         /// <returns>The matching files.</returns>
         public async Task<IEnumerable<File>> SearchAsync(SearchQuery query)
         {
-            // sanitize the query string. there's probably more to it than this.
-            var text = query.Query
-                .Replace("/", " ")
+            string Clean(string str) => str.Replace("/", " ")
                 .Replace("\\", " ")
                 .Replace(":", " ")
                 .Replace("\"", " ")
                 .Replace("'", "''");
 
-            var tokens = text.Split(' ').Select(token => $"\"{token}\"");
+            var match = string.Join(" AND ", query.Terms.Select(token => $"\"{Clean(token)}\""));
+            var exclusions = string.Join(" OR ", query.Exclusions.Select(exclusion => $"\"{Clean(exclusion)}\""));
 
-            var sql = $"SELECT * FROM cache WHERE cache MATCH '{string.Join(" AND ", tokens)}'";
+            var sql = $"SELECT * FROM cache WHERE cache MATCH '({match}) {(query.Exclusions.Any() ? $"NOT ({exclusions})" : string.Empty)}'";
 
             try
             {
