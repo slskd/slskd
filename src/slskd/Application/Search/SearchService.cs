45da80db80aff2930c182544c7ae2a268a25a768
JP Dillingham <jpdillingham@gmail.com>
2021-08-26 09:20:58 -0500

    remove unused import

--- src/slskd/Application/Search/SearchService.cs
+++ src/slskd/Application/Search/SearchService.cs
@@ -24,7 +24,6 @@ namespace slskd.Search
     using System.Collections.Generic;
     using System.Linq;
     using System.Linq.Expressions;
-    using System.Text.RegularExpressions;
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.EntityFrameworkCore;
