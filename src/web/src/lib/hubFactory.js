32a9a0f21e93cb0e172c00f0864a17b74d3dfa3b
JP Dillingham <jpdillingham@gmail.com>
2021-10-03 11:52:43 -0500

    extend hubFactory

--- src/web/src/lib/hubFactory.js
+++ src/web/src/lib/hubFactory.js
@@ -7,12 +7,14 @@ import {
 import { rootUrl } from '../config';
 import * as session from '../lib/session';
 
-export const createApplicationHubConnection = () =>
+export const createHubConnection = ({ url }) => 
   new HubConnectionBuilder()
-    .withUrl(`${rootUrl}/hub/application`, {
+    .withUrl(url, {
         accessTokenFactory: session.getToken
     })
     .withAutomaticReconnect()
     .withHubProtocol(new JsonHubProtocol())
     .configureLogging(LogLevel.Information)
     .build();
+
+export const createApplicationHubConnection = () => createHubConnection({ url: `${rootUrl}/hub/application` });
