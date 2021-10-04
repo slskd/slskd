090b4ba79d0859e90d71671c62da96c3f8a6deb2
JP Dillingham <jpdillingham@gmail.com>
2021-10-04 18:51:44 -0500

    add LogsController and DTO

---
+++ src/slskd/Core/API/Controllers/LogsController.cs
@@ -0,0 +1,62 @@
+﻿// <copyright file="LogsController.cs" company="slskd Team">
+//     Copyright (c) slskd Team. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU Affero General Public License as published
+//     by the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU Affero General Public License for more details.
+//
+//     You should have received a copy of the GNU Affero General Public License
+//     along with this program.  If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace slskd.Core.API
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Serilog.Formatting.Compact;
+    using Serilog.Formatting.Display;
+    using Serilog.Formatting.Json;
+
+    /// <summary>
+    ///     Application.
+    /// </summary>
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("0")]
+    [ApiController]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    public class LogsController : ControllerBase
+    {
+        /// <summary>
+        ///     Gets the last few application logs.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Authorize]
+        public IActionResult Logs()
+        {
+            var fmt = new CompactJsonFormatter();
+            var logs = Program.LogBuffer.ToList();
+
+            List<string> o = new List<string>();
+
+            foreach (var log in logs)
+            {
+                var renderSpace = new StringWriter();
+                fmt.Format(log, renderSpace);
+                o.Add(renderSpace.ToString());
+            }
+
+            return Ok(o);
+        }
+    }
+}
---
+++ src/slskd/Core/API/DTO/Log.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace slskd.Core.API.DTO
+{
+    public class Log
+    {
+    }
+}
