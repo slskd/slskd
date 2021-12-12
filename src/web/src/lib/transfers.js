b3b19f1a563a20693785394b7f9bd0de2720531a
JP Dillingham <jpdillingham@gmail.com>
2021-12-12 12:03:48 -0600

    send a list of requests when posting downloads

--- src/web/src/lib/transfers.js
+++ src/web/src/lib/transfers.js
@@ -11,8 +11,8 @@ export const getAll = async ({ direction }) => {
   return response;
 };
 
-export const download = ({ username, filename, size }) => {
-  return api.post(`/transfers/downloads/${username}`, { filename, size });
+export const download = ({ username, files = [] }) => {
+  return api.post(`/transfers/downloads/${username}`, files);
 };
 
 export const cancel = ({ direction, username, id, remove = false }) => {
