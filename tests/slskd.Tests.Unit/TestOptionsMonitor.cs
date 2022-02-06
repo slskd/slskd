4ab8c662d2fa04049fdb57a2b0a300dd4239b8b6
JP Dillingham <jpdillingham@gmail.com>
2022-02-06 17:10:31 -0600

    add some things to make testing easier

---
+++ tests/slskd.Tests.Unit/TestOptionsMonitor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Extensions.Options;
+using Moq;
+
+namespace slskd.Tests.Unit
+{
+    public class TestOptionsMonitor<TOptions> : IOptionsMonitor<TOptions>
+    {
+        private Action<TOptions, string> _listener;
+
+        public TestOptionsMonitor(TOptions currentValue)
+        {
+            CurrentValue = currentValue;
+        }
+
+        public TOptions CurrentValue { get; private set; }
+
+        public TOptions Get(string name)
+        {
+            return CurrentValue;
+        }
+
+        public void Set(TOptions value)
+        {
+            CurrentValue = value;
+            _listener.Invoke(value, null);
+        }
+
+        public IDisposable OnChange(Action<TOptions, string> listener)
+        {
+            _listener = listener;
+            return new Mock<IDisposable>().Object;
+        }
+
+        public void RaiseOnchange(TOptions options)
+        {
+            _listener(options, null);
+        }
+    }
+}
