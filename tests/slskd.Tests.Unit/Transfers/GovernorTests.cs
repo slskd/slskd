80b2b5f675a857e446c3a3dcc4cb3ad3c8b70a31
JP Dillingham <jpdillingham@gmail.com>
2022-02-06 17:10:46 -0600

    add tests for bucket configuration

---
+++ tests/slskd.Tests.Unit/Transfers/GovernorTests.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using AutoFixture.Xunit2;
+using Moq;
+using slskd.Transfers;
+using slskd.Users;
+using Xunit;
+
+namespace slskd.Tests.Unit.Transfers
+{
+    public class GovernorTests
+    {
+        [Fact]
+        private void Instantiates_With_BuiltIn_Buckets()
+        {
+            var (governor, _) = GetFixture();
+
+            var buckets = governor.GetProperty<Dictionary<string, ITokenBucket>>("TokenBuckets");
+
+            Assert.Equal(3, buckets.Count);
+            Assert.True(buckets.ContainsKey(Application.PriviledgedGroup));
+            Assert.True(buckets.ContainsKey(Application.DefaultGroup));
+            Assert.True(buckets.ContainsKey(Application.LeecherGroup));
+        }
+
+        [Theory, AutoData]
+        private void Instantiates_With_User_Defined_Buckets(string group1, string group2)
+        {
+            var options = new Options()
+            {
+                Groups = new Options.GroupsOptions()
+                {
+                    UserDefined = new Dictionary<string, Options.GroupsOptions.UserDefinedOptions>()
+                    {
+                        { group1, new Options.GroupsOptions.UserDefinedOptions() },
+                        { group2, new Options.GroupsOptions.UserDefinedOptions() },
+                    }
+                }
+            };
+
+            var (governor, _) = GetFixture(options);
+
+            var buckets = governor.GetProperty<Dictionary<string, ITokenBucket>>("TokenBuckets");
+
+            Assert.Equal(5, buckets.Count);
+            Assert.True(buckets.ContainsKey(group1));
+            Assert.True(buckets.ContainsKey(group2));
+        }
+
+        [Theory, AutoData]
+        private void Reconfigures_Buckets_When_Options_change(string group)
+        {
+            var options = new Options()
+            {
+                Groups = new Options.GroupsOptions()
+                {
+                    UserDefined = new Dictionary<string, Options.GroupsOptions.UserDefinedOptions>()
+                    {
+                        { group, new Options.GroupsOptions.UserDefinedOptions() },
+                    }
+                }
+            };
+
+            // do not pass options; only default buckets
+            var (governor, mocks) = GetFixture();
+
+            var buckets = governor.GetProperty<Dictionary<string, ITokenBucket>>("TokenBuckets");
+
+            // ensure only default buckets are created
+            Assert.Equal(3, buckets.Count);
+            Assert.False(buckets.ContainsKey(group));
+
+            // reconfigure with options
+            mocks.OptionsMonitor.RaiseOnchange(options);
+
+            // grab the new copy of buckets
+            buckets = governor.GetProperty<Dictionary<string, ITokenBucket>>("TokenBuckets");
+
+            Assert.Equal(4, buckets.Count);
+            Assert.True(buckets.ContainsKey(group));
+        }
+
+        private (Governor governor, Mocks mocks) GetFixture(Options options = null)
+        {
+            var mocks = new Mocks(options);
+            var governor = new Governor(
+                mocks.UserService.Object,
+                mocks.OptionsMonitor);
+
+            return (governor, mocks);
+        }
+
+        private class Mocks
+        {
+            public Mocks(Options options = null)
+            {
+                OptionsMonitor = new TestOptionsMonitor<Options>(options ?? new Options());
+            }
+
+            public Mock<IUserService> UserService { get; } = new Mock<IUserService>();
+            public TestOptionsMonitor<Options> OptionsMonitor { get; init; }
+        }
+    }
+}
