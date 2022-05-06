7e9be73142ca1ddb3f5c7523c81367e6db55a895
JP Dillingham <jpdillingham@gmail.com>
2022-05-05 20:44:02 -0500

    docs

--- src/slskd/Common/Middleware/HTMLInjectionMiddleware.cs
+++ src/slskd/Common/Middleware/HTMLInjectionMiddleware.cs
@@ -1,4 +1,21 @@
-﻿namespace slskd
+﻿// <copyright file="HTMLInjectionMiddleware.cs" company="slskd Team">
+//     Copyright (c) slskd Team. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU Affero General Public License as published
+//     by the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU Affero General Public License for more details.
+//
+//     You should have received a copy of the GNU Affero General Public License
+//     along with this program.  If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace slskd
 {
     using System;
     using System.Linq;
@@ -7,16 +24,33 @@
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Http;
 
+    /// <summary>
+    ///     Adds the <see cref="HTMLInjectionMiddleware"/>.
+    /// </summary>
     public static class HTMLInjectionMiddlewareExtensions
     {
+        /// <summary>
+        ///     Injects the specified <paramref name="html"/> into html files.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="html"></param>
+        /// <returns></returns>
         public static IApplicationBuilder UseHTMLInjection(this IApplicationBuilder builder, string html)
         {
             return builder.UseMiddleware<HTMLInjectionMiddleware>(html);
         }
     }
 
+    /// <summary>
+    ///     Injects the specified HTML into html files.
+    /// </summary>
     public class HTMLInjectionMiddleware
     {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="HTMLInjectionMiddleware"/> class.
+        /// </summary>
+        /// <param name="next"></param>
+        /// <param name="html"></param>
         public HTMLInjectionMiddleware(RequestDelegate next, string html)
         {
             Next = next;
@@ -26,6 +60,11 @@
         private string HTML { get; }
         private RequestDelegate Next { get; }
 
+        /// <summary>
+        ///     Executes this middleware, returning the contents of the requested HTML file with the specified HTML appended.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public async Task InvokeAsync(HttpContext context)
         {
             context.Request.Headers.TryGetValue("accept", out var accept);
