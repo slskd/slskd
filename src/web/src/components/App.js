2de400676d742980e2ef9c889e3f47db647f8c24
JP Dillingham <jpdillingham@gmail.com>
2022-05-17 21:32:48 -0500

    add toast container

--- src/web/src/components/App.js
+++ src/web/src/components/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Route, Link, Switch, Redirect } from "react-router-dom";
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import * as session from '../lib/session';
 import { connect, disconnect } from '../lib/server';
 import { urlBase } from '../config';
@@ -137,124 +140,137 @@ class App extends Component {
     }
 
     return (
-      <Sidebar.Pushable as={Segment} className='app'>
-        <Sidebar
-          className='navigation'
-          as={Menu} 
-          animation='overlay' 
-          icon='labeled' 
-          inverted 
-          horizontal='true'
-          direction='top' 
-          visible width='thin'
-        >
-          {version.isCanary && <Menu.Item>
-            <Icon name='flask' color='yellow'/>Canary
-          </Menu.Item>}
-          <Link to={`${urlBase}/searches`}>
-            <Menu.Item>
-              <Icon name='search'/>Search
-            </Menu.Item>
-          </Link>
-          <Link to={`${urlBase}/downloads`}>
-            <Menu.Item>
-              <Icon name='download'/>Downloads
-            </Menu.Item>
-          </Link>
-          <Link to={`${urlBase}/uploads`}>
-            <Menu.Item>
-              <Icon name='upload'/>Uploads
-            </Menu.Item>
-          </Link>
-          <Link to={`${urlBase}/rooms`}>
-            <Menu.Item>
-              <Icon name='comments'/>Rooms
-            </Menu.Item>
-          </Link>
-          <Link to={`${urlBase}/chat`}>
-            <Menu.Item>
-              <Icon name='comment'/>Chat
-            </Menu.Item>
-          </Link>
-          <Link to={`${urlBase}/users`}>
-            <Menu.Item>
-              <Icon name='users'/>Users
-            </Menu.Item>
-          </Link>
-          <Link to={`${urlBase}/browse`}>
-            <Menu.Item>
-              <Icon name='folder open'/>Browse
-            </Menu.Item>
-          </Link>
-          <Menu className='right' inverted>
-            {server?.isConnected && <Menu.Item
-              onClick={() => disconnect()}
-            >
-              <Icon name='plug' color='green'/>Connected
+      <>
+        <Sidebar.Pushable as={Segment} className='app'>
+          <Sidebar
+            className='navigation'
+            as={Menu} 
+            animation='overlay' 
+            icon='labeled' 
+            inverted 
+            horizontal='true'
+            direction='top' 
+            visible width='thin'
+          >
+            {version.isCanary && <Menu.Item>
+              <Icon name='flask' color='yellow'/>Canary
             </Menu.Item>}
-            {(!server?.isConnected) && <Menu.Item 
-              onClick={() => connect()}
-            >
-              <Icon.Group className='menu-icon-group'>
-                <Icon name='plug' color='grey'/>
-                <Icon name='close' color='red' corner='bottom right' className='menu-icon-no-shadow'/>
-              </Icon.Group>Disconnected
-            </Menu.Item>}
-            {isUpdateAvailable && <Modal
-              trigger={<Menu.Item position='right'>
-                <Icon.Group className='menu-icon-group'>
-                  <Icon name='bullhorn' color='yellow'/>
-                </Icon.Group>New Version!
-              </Menu.Item>}
-              centered
-              closeIcon
-              size='mini'
-            >
-              <Modal.Header>New Version!</Modal.Header>
-              <Modal.Content>
-                <p>You are currently running version <strong>{current}</strong> while version <strong>{latest}</strong> is available.</p>
-              </Modal.Content>
-              <Modal.Actions>
-                <Button
-                  style={{marginLeft: 0}}
-                  primary 
-                  fluid
-                  href="https://github.com/slskd/slskd/releases">See Release Notes</Button>
-              </Modal.Actions>
-            </Modal>}
-            <Link to={`${urlBase}/system`}>
+            <Link to={`${urlBase}/searches`}>
               <Menu.Item>
-                <Icon name='cogs'/>System
+                <Icon name='search'/>Search
               </Menu.Item>
             </Link>
-            {session.isLoggedIn() && <Modal
-              trigger={
+            <Link to={`${urlBase}/downloads`}>
+              <Menu.Item>
+                <Icon name='download'/>Downloads
+              </Menu.Item>
+            </Link>
+            <Link to={`${urlBase}/uploads`}>
+              <Menu.Item>
+                <Icon name='upload'/>Uploads
+              </Menu.Item>
+            </Link>
+            <Link to={`${urlBase}/rooms`}>
+              <Menu.Item>
+                <Icon name='comments'/>Rooms
+              </Menu.Item>
+            </Link>
+            <Link to={`${urlBase}/chat`}>
+              <Menu.Item>
+                <Icon name='comment'/>Chat
+              </Menu.Item>
+            </Link>
+            <Link to={`${urlBase}/users`}>
+              <Menu.Item>
+                <Icon name='users'/>Users
+              </Menu.Item>
+            </Link>
+            <Link to={`${urlBase}/browse`}>
+              <Menu.Item>
+                <Icon name='folder open'/>Browse
+              </Menu.Item>
+            </Link>
+            <Menu className='right' inverted>
+              {server?.isConnected && <Menu.Item
+                onClick={() => disconnect()}
+              >
+                <Icon name='plug' color='green'/>Connected
+              </Menu.Item>}
+              {(!server?.isConnected) && <Menu.Item 
+                onClick={() => connect()}
+              >
+                <Icon.Group className='menu-icon-group'>
+                  <Icon name='plug' color='grey'/>
+                  <Icon name='close' color='red' corner='bottom right' className='menu-icon-no-shadow'/>
+                </Icon.Group>Disconnected
+              </Menu.Item>}
+              {isUpdateAvailable && <Modal
+                trigger={<Menu.Item position='right'>
+                  <Icon.Group className='menu-icon-group'>
+                    <Icon name='bullhorn' color='yellow'/>
+                  </Icon.Group>New Version!
+                </Menu.Item>}
+                centered
+                closeIcon
+                size='mini'
+              >
+                <Modal.Header>New Version!</Modal.Header>
+                <Modal.Content>
+                  <p>You are currently running version <strong>{current}</strong> while version <strong>{latest}</strong> is available.</p>
+                </Modal.Content>
+                <Modal.Actions>
+                  <Button
+                    style={{marginLeft: 0}}
+                    primary 
+                    fluid
+                    href="https://github.com/slskd/slskd/releases">See Release Notes</Button>
+                </Modal.Actions>
+              </Modal>}
+              <Link to={`${urlBase}/system`}>
                 <Menu.Item>
-                  <Icon name='sign-out'/>Log Out
+                  <Icon name='cogs'/>System
                 </Menu.Item>
-              }
-              centered
-              size='mini'
-              header={<Header icon='sign-out' content='Confirm Log Out' />}
-              content='Are you sure you want to log out?'
-              actions={['Cancel', { key: 'done', content: 'Log Out', negative: true, onClick: this.logout }]}
-            />}
-          </Menu>
-        </Sidebar>
-        <Sidebar.Pusher className='app-content'>
-          <Switch>
-            <Route path={`${urlBase}/searches/:id?`} render={(props) => this.withTokenCheck(<div className='search-container'><Searches {...props}/></div>)}/>
-            <Route path={`${urlBase}/browse`} render={(props) => this.withTokenCheck(<Browse {...props}/>)}/>
-            <Route path={`${urlBase}/users`} render={(props) => this.withTokenCheck(<Users {...props}/>)}/>
-            <Route path={`${urlBase}/chat`} render={(props) => this.withTokenCheck(<Chat {...props}/>)}/>
-            <Route path={`${urlBase}/rooms`} render={(props) => this.withTokenCheck(<Rooms {...props}/>)}/>
-            <Route path={`${urlBase}/uploads`} render={(props) => this.withTokenCheck(<Transfers {...props} direction='upload'/>)}/>
-            <Route path={`${urlBase}/downloads`} render={(props) => this.withTokenCheck(<Transfers {...props} direction='download'/>)}/>
-            <Route path={`${urlBase}/system/:tab?`} render={(props) => this.withTokenCheck(<System {...props} state={applicationState} options={applicationOptions}/>)}/>
-            <Redirect from='*' to={`${urlBase}/searches`}/>
-          </Switch>
-        </Sidebar.Pusher>
-      </Sidebar.Pushable>
+              </Link>
+              {session.isLoggedIn() && <Modal
+                trigger={
+                  <Menu.Item>
+                    <Icon name='sign-out'/>Log Out
+                  </Menu.Item>
+                }
+                centered
+                size='mini'
+                header={<Header icon='sign-out' content='Confirm Log Out' />}
+                content='Are you sure you want to log out?'
+                actions={['Cancel', { key: 'done', content: 'Log Out', negative: true, onClick: this.logout }]}
+              />}
+            </Menu>
+          </Sidebar>
+          <Sidebar.Pusher className='app-content'>
+            <Switch>
+              <Route path={`${urlBase}/searches/:id?`} render={(props) => this.withTokenCheck(<div className='search-container'><Searches {...props}/></div>)}/>
+              <Route path={`${urlBase}/browse`} render={(props) => this.withTokenCheck(<Browse {...props}/>)}/>
+              <Route path={`${urlBase}/users`} render={(props) => this.withTokenCheck(<Users {...props}/>)}/>
+              <Route path={`${urlBase}/chat`} render={(props) => this.withTokenCheck(<Chat {...props}/>)}/>
+              <Route path={`${urlBase}/rooms`} render={(props) => this.withTokenCheck(<Rooms {...props}/>)}/>
+              <Route path={`${urlBase}/uploads`} render={(props) => this.withTokenCheck(<Transfers {...props} direction='upload'/>)}/>
+              <Route path={`${urlBase}/downloads`} render={(props) => this.withTokenCheck(<Transfers {...props} direction='download'/>)}/>
+              <Route path={`${urlBase}/system/:tab?`} render={(props) => this.withTokenCheck(<System {...props} state={applicationState} options={applicationOptions}/>)}/>
+              <Redirect from='*' to={`${urlBase}/searches`}/>
+            </Switch>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+        <ToastContainer
+          position="bottom-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable={false}
+          pauseOnHover
+        />
+      </>
     )
   };
 };
