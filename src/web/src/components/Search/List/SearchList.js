86c510a31fa5e279da49b834808c2a81c6782c74
JP Dillingham <jpdillingham@gmail.com>
2022-05-27 00:59:48 -0500

    tidy up

--- src/web/src/components/Search/List/SearchList.js
+++ src/web/src/components/Search/List/SearchList.js
@@ -27,33 +27,29 @@ const SearchList = ({
             connecting={connecting && <Loader active inline='centered' size='small'/>}
             error={error && <ErrorSegment caption={error} />}
           >
-            <List>
-              <List.Item>
-                <Table size='large'>
-                  <Table.Header>
-                    <Table.Row>
-                      <Table.HeaderCell className="search-list-action"><Icon name="info circle"/></Table.HeaderCell>
-                      <Table.HeaderCell className="search-list-phrase">Search</Table.HeaderCell>
-                      <Table.HeaderCell className="search-list-files">Files</Table.HeaderCell>
-                      <Table.HeaderCell className="search-list-locked">Locked</Table.HeaderCell>
-                      <Table.HeaderCell className="search-list-responses">Responses</Table.HeaderCell>
-                      <Table.HeaderCell className="search-list-started">Ended</Table.HeaderCell>
-                      <Table.HeaderCell className="search-list-action"></Table.HeaderCell>
-                    </Table.Row>
-                  </Table.Header>
-                  <Table.Body>
-                    {Object.values(searches)
-                      .sort((a, b) => (new Date(b.startedAt) - new Date(a.startedAt)))
-                      .map((search, index) => <SearchListRow
-                        search={search}
-                        key={index}
-                        onRemove={onRemove}
-                        onStop={onStop}
-                      />)}
-                  </Table.Body>
-                </Table>
-              </List.Item>
-            </List>
+            <Table size='large'>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell className="search-list-action"><Icon name="info circle"/></Table.HeaderCell>
+                  <Table.HeaderCell className="search-list-phrase">Search</Table.HeaderCell>
+                  <Table.HeaderCell className="search-list-files">Files</Table.HeaderCell>
+                  <Table.HeaderCell className="search-list-locked">Locked</Table.HeaderCell>
+                  <Table.HeaderCell className="search-list-responses">Responses</Table.HeaderCell>
+                  <Table.HeaderCell className="search-list-started">Ended</Table.HeaderCell>
+                  <Table.HeaderCell className="search-list-action"></Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                {Object.values(searches)
+                  .sort((a, b) => (new Date(b.startedAt) - new Date(a.startedAt)))
+                  .map((search, index) => <SearchListRow
+                    search={search}
+                    key={index}
+                    onRemove={onRemove}
+                    onStop={onStop}
+                  />)}
+              </Table.Body>
+            </Table>
           </Switch>
         </div>
       </Card.Content>
