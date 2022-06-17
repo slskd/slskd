981fc00ccf629071527564d2b5c69333fa25c119
Matt Goldspink <mattgoldspink1@gmail.com>
2022-06-17 06:30:59 -0700

    Reverting back to a functional component and using useState instead.

--- src/web/src/components/Shared/FileList.js
+++ src/web/src/components/Shared/FileList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import { formatSeconds, formatBytes, getFileName, formatAttributes } from '../../lib/util';
 
@@ -10,85 +10,77 @@ import {
   Checkbox,
 } from 'semantic-ui-react';
 
-class FileList extends React.Component {
-  state = {
-    isFolded: false,
-  };
 
-  toggleFolded = () => {
-    if (!this.props.locked) {
-      this.setState({'isFolded': !this.state.isFolded});
-    }
-  };
+const FileList = ({ directoryName, files, locked, onSelectionChange, disabled, onClose }) => {
+  const [folded, setFolded] = useState(false);
 
-  render() {
-    return (
-      <div style={{opacity: this.props.locked ? 0.5 : 1}}>
-        <Header 
-          size='small' 
-          className='filelist-header'
-        >
-          <div>
-            <Icon size='large' 
-              link={!this.props.locked}
-              name={this.props.locked ? 'lock' : this.state.isFolded ? 'folder' : 'folder open'} 
-              onClick={() => this.toggleFolded()}/>
-            {this.props.directoryName}
-        
-            {!!this.props.onClose && <Icon 
-              className='close-button' 
-              name='close' 
-              color='red'
-              link
-              onClick={() => this.props.onClose()}
-            />}
-          </div>
-        </Header>
-        {!this.state.isFolded && this.props.files && this.props.files.length > 0 && <List>
-          <List.Item>
-            <Table>
-              <Table.Header>
-                <Table.Row>
-                  <Table.HeaderCell className='filelist-selector'>
+  return (
+    <div style={{opacity: locked ? 0.5 : 1}}>
+      <Header 
+        size='small' 
+        className='filelist-header'
+      >
+        <div>
+          <Icon 
+            size='large' 
+            link={!locked} 
+            name={locked ? 'lock' : folded ? 'folder' : 'folder open'}
+            onClick={() => setFolded(!folded)}/>
+          {directoryName}
+      
+          {!!onClose && <Icon 
+            className='close-button' 
+            name='close' 
+            color='red'
+            link
+            onClick={() => onClose()}
+          />}
+        </div>
+      </Header>
+      {!folded && files && files.length > 0 && <List>
+        <List.Item>
+          <Table>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell className='filelist-selector'>
+                  <Checkbox 
+                    fitted
+                    onChange={(event, data) => files.map(f => onSelectionChange(f, data.checked))}
+                    checked={files.filter(f => !f.selected).length === 0}
+                    disabled={disabled}
+                  />
+                </Table.HeaderCell>
+                <Table.HeaderCell className='filelist-filename'>File</Table.HeaderCell>
+                <Table.HeaderCell className='filelist-size'>Size</Table.HeaderCell>
+                <Table.HeaderCell className='filelist-attributes'>Attributes</Table.HeaderCell>
+                <Table.HeaderCell className='filelist-length'>Length</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {files.sort((a, b) => a.filename > b.filename ? 1 : -1).map((f, i) => 
+                <Table.Row key={i}>
+                  <Table.Cell className='filelist-selector'>
                     <Checkbox 
-                      fitted
-                      onChange={(event, data) => this.props.files.map(f => 
-                        this.props.onSelectionChange(f, data.checked))}
-                      checked={this.props.files.filter(f => !f.selected).length === 0}
-                      disabled={this.props.disabled}
+                      fitted 
+                      onChange={(event, data) => onSelectionChange(f, data.checked)}
+                      checked={f.selected}
+                      disabled={disabled}
                     />
-                  </Table.HeaderCell>
-                  <Table.HeaderCell className='filelist-filename'>File</Table.HeaderCell>
-                  <Table.HeaderCell className='filelist-size'>Size</Table.HeaderCell>
-                  <Table.HeaderCell className='filelist-attributes'>Attributes</Table.HeaderCell>
-                  <Table.HeaderCell className='filelist-length'>Length</Table.HeaderCell>
+                  </Table.Cell>
+                  <Table.Cell className='filelist-filename'>
+                    {locked ? <Icon name='lock' /> : ''}{getFileName(f.filename)}
+                  </Table.Cell>
+                  <Table.Cell className='filelist-size'>{formatBytes(f.size)}</Table.Cell>
+                  <Table.Cell className='filelist-attributes'>{formatAttributes(f)}</Table.Cell>
+                  <Table.Cell className='filelist-length'>{formatSeconds(f.length)}</Table.Cell>
                 </Table.Row>
-              </Table.Header>
-              <Table.Body>
-                {this.props.files.sort((a, b) => a.filename > b.filename ? 1 : -1).map((f, i) => 
-                  <Table.Row key={i}>
-                    <Table.Cell className='filelist-selector'>
-                      <Checkbox 
-                        fitted 
-                        onChange={(event, data) => this.props.onSelectionChange(f, data.checked)}
-                        checked={f.selected}
-                        disabled={this.props.disabled}
-                      />
-                    </Table.Cell>
-                    <Table.Cell className='filelist-filename'>
-                      {this.props.locked ? <Icon name='lock' /> : ''}{getFileName(f.filename)}
-                    </Table.Cell>
-                    <Table.Cell className='filelist-size'>{formatBytes(f.size)}</Table.Cell>
-                    <Table.Cell className='filelist-attributes'>{formatAttributes(f)}</Table.Cell>
-                    <Table.Cell className='filelist-length'>{formatSeconds(f.length)}</Table.Cell>
-                  </Table.Row>
-                )}
-              </Table.Body>
-            </Table>
-          </List.Item>
-        </List>}
-      </div>);
-  }
-}
+              )}
+            </Table.Body>
+          </Table>
+        </List.Item>
+      </List>}
+    </div>
+  );
+};
 
 export default FileList;
