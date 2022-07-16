1f482b465ac371b44c1dfa118b0a5e7e61617ddb
JP Dillingham <jpdillingham@gmail.com>
2022-07-16 16:06:30 -0500

    add WithSoulseekTransfer extension

---
+++ src/slskd/Transfers/Extensions.cs
@@ -0,0 +1,26 @@
+﻿namespace slskd.Transfers
+{
+    public static class Extensions
+    {
+        public static Transfer WithSoulseekTransfer(this Transfer transfer, Soulseek.Transfer t)
+        {
+            return new Transfer()
+            {
+                Id = transfer.Id,
+                Username = transfer.Username,
+                Direction = transfer.Direction,
+                Filename = transfer.Filename,
+                Size = transfer.Size,
+                StartOffset = transfer.StartOffset,
+                State = t.State,
+                RequestedAt = transfer.RequestedAt,
+                EnqueuedAt = transfer.EnqueuedAt,
+                StartedAt = t.StartTime,
+                EndedAt = t.EndTime,
+                BytesTransferred = t.BytesTransferred,
+                AverageSpeed = t.AverageSpeed,
+                Exception = t.Exception,
+            };
+        }
+    }
+}
