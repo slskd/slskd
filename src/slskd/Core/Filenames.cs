f8d129bb570af056e9646582089895ad7f775faa
JP Dillingham <jp@dillingham.me>
2022-09-07 22:21:19 -0500

    add Filenames

---
+++ src/slskd/Core/Filenames.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="Filenames.cs" company="slskd Team">
+//     Copyright (c) slskd Team. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU Affero General Public License as published
+//     by the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU Affero General Public License for more details.
+//
+//     You should have received a copy of the GNU Affero General Public License
+//     along with this program.  If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace slskd
+{
+    /// <summary>
+    ///     Application files.
+    /// </summary>
+    public static class Filenames
+    {
+        public static readonly string TransfersDb = "transfers.db";
+        public static readonly string SharesDb = "shares.db";
+        public static readonly string SearchDb = "search.db";
+        public static readonly string BrowseCache = "browse.cache";
+    }
+}
