374df66d4cc4f9f55a2a0a3e69b50260525060ce
JP Dillingham <jp@dillingham.me>
2022-09-25 14:22:01 -0500

    add a button to cancel a scan, show files and folder counts

--- src/web/src/components/System/Shares/index.js
+++ src/web/src/components/System/Shares/index.js
@@ -19,10 +19,11 @@ import ShareTable from './ShareTable';
 
 const Index = ({ state = {} } = {}) => {
   const [loading, setLoading] = useState(true);
+  const [working, setWorking] = useState(false);
   const [shares, setShares] = useState([]);
   const [modal, setModal] = useState(false);
 
-  const { scanning, scanProgress, scanPending } = state;
+  const { scanning, scanProgress, scanPending, directories, files } = state;
   const scanned = !scanning;
 
   useEffect(() => {
@@ -47,30 +48,76 @@ const Index = ({ state = {} } = {}) => {
     }
   };
 
+  const rescan = async () => {
+    try {
+      setWorking(true);
+      await sharesLib.rescan();
+    } catch (error) {
+      console.error(error);
+      toast.error(error?.response?.data ?? error?.message ?? error);
+    } finally {
+      setWorking(false);
+    }
+  };
+
+  const cancel = async () => {
+    try {
+      setWorking(true);
+      await sharesLib.cancel();
+    } catch (error) {
+      console.error(error);
+      toast.error(error?.response?.data ?? error?.message ?? error ?? 'Failed to cancel the scan');
+    } finally {
+      setWorking(false);      
+    }
+  };
+
+  const ScanButton = () => <ShrinkableButton
+    primary={!scanPending}
+    color={scanPending ? 'yellow' : undefined}
+    icon='refresh'
+    loading={working}
+    disabled={working}
+    mediaQuery={'(max-width: 516px)'} 
+    onClick={() => rescan()}
+  >Rescan Shares</ShrinkableButton>;
+
+  const CancelButton = () => <ShrinkableButton
+    color='red'
+    icon='x'
+    disabled={working}
+    mediaQuery={'(max-width: 516px)'}
+    onClick={() => cancel()}
+  >Cancel Scan</ShrinkableButton>;
+
   const shared = shares.filter(share => !share.isExcluded);
   const excluded = shares.filter(share => share.isExcluded);
 
   return (
     <>
-      <div className="header-buttons">
-        <ShrinkableButton
-          primary={!scanPending}
-          color={scanPending ? 'yellow' : undefined}
-          icon='refresh'
-          loading={scanning}
-          disabled={scanning || loading}
-          mediaQuery={'(max-width: 516px)'} 
-          onClick={() => sharesLib.rescan()}
-        >{scanning ? 'Scanning shares' : 'Rescan Shares'}</ShrinkableButton>
-      </div>
-      <Divider/>
       <Switch
-        filling={(loading || scanning) && <LoaderSegment>{Math.round(scanProgress * 100)}%</LoaderSegment>}
+        loading={loading && <LoaderSegment/>}
       >
-        <ShareTable shares={shared} onClick={setModal}/>
-        <ExclusionTable exclusions={excluded}/>
+        <div className="header-buttons">
+          <Switch
+            scanning={scanning && <CancelButton/>}
+          >
+            <ScanButton/>
+          </Switch>
+        </div>
+        <Divider/>
+        <Switch
+          filling={scanning && <LoaderSegment>
+            <div>
+              <div>{Math.round(scanProgress * 100)}%</div>
+              <div className='share-scan-detail'>Found {files} files in {directories} directories</div>
+            </div></LoaderSegment>}
+        >
+          <ShareTable shares={shared} onClick={setModal}/>
+          <ExclusionTable exclusions={excluded}/>
+        </Switch>
+        <ContentsModal share={modal} onClose={() => setModal(false)}/>
       </Switch>
-      <ContentsModal share={modal} onClose={() => setModal(false)}/>
     </>    
   );
 };
