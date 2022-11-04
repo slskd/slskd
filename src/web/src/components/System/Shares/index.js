964d50bc5156069b4404f29f823a4a21bd15ffe5
JP Dillingham <jp@dillingham.me>
2022-11-04 09:13:26 -0500

    remove unused import

--- src/web/src/components/System/Shares/index.js
+++ src/web/src/components/System/Shares/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
-import { flatten } from 'lodash';
 
 import * as sharesLib from '../../../lib/shares';
 import { 
