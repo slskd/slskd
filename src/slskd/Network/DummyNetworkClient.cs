802483716d99aec0e6dc180879022ff00fb0d7e3
JP Dillingham <jpdillingham@gmail.com>
2022-11-06 17:16:23 -0600

    add DummyNetworkClient

---
+++ src/slskd/Network/DummyNetworkClient.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="DummyNetworkClient.cs" company="slskd Team">
+//     Copyright (c) slskd Team. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU Affero General Public License as published
+//     by the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU Affero General Public License for more details.
+//
+//     You should have received a copy of the GNU Affero General Public License
+//     along with this program.  If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace slskd.Network
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class DummyNetworkClient : INetworkClient
+    {
+        public IStateMonitor<NetworkClientState> StateMonitor => new ManagedState<NetworkClientState>();
+
+        public void Dispose() { }
+
+        public Task StartAsync(CancellationToken cancellationToken = default) => Task.CompletedTask;
+
+        public Task StopAsync(CancellationToken cancellationToken = default) => Task.CompletedTask;
+
+        public Task SynchronizeAsync(CancellationToken cancellationToken = default) => Task.CompletedTask;
+    }
+}
