6de4763df03c5bcbfceb60df25e6e8b36f2e32ad
JP Dillingham <jp@dillingham.me>
2023-02-04 12:19:46 -0600

    Revert "Merge pull request #874 from slskd/revert-862-port"
    This reverts commit 02364ac34c7ebabbb348a0806a36f6bc2153a609, reversing
    changes made to 3162a3f7964e924372a7744f4ce02951894dfe38.

--- src/web/src/config.js
+++ src/web/src/config.js
@@ -1,5 +1,6 @@
 const urlBase = (window.urlBase === '/' ? '' : window.urlBase) || '';
-const rootUrl = process.env.NODE_ENV === 'production' ? urlBase : `http://localhost:5000${urlBase}`;
+const devPort = window.port ?? 5030;
+const rootUrl = process.env.NODE_ENV === 'production' ? urlBase : `http://localhost:${devPort}${urlBase}`;
 const apiBaseUrl = `${rootUrl}/api/v0`;
 const hubBaseUrl = `${rootUrl}/hub`;
 const tokenKey = 'slskd-token';
