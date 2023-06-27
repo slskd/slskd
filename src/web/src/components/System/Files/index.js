e629106b6ff136910422e857466714ceb420f841
JP Dillingham <jp@dillingham.me>
2023-06-27 09:07:24 -0500

    stub files tab

---
+++ src/web/src/components/System/Files/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header } from 'semantic-ui-react';
+
+const Files = () => {
+  return (
+    <div>
+      <Header as='h3' className='file-management-header'>Files</Header>
+    </div>
+  );
+};
+
+export default Files;
