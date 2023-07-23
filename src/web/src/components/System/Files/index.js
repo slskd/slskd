2ff09498b33c7b2e0d8318cc85e09da5025e0024
JP Dillingham <jp@dillingham.me>
2023-07-22 23:01:10 -0500

    track active tab

--- src/web/src/components/System/Files/index.js
+++ src/web/src/components/System/Files/index.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Tab } from 'semantic-ui-react';
 
+import './Files.css';
 import Explorer from './Explorer';
 
 const Files = () => {
+  const [tab, setTab] = useState('downloads');
+
   const panes = [
     { 
       route: 'downloads',
       menuItem: 'Downloads',
-      render: () => <Tab.Pane><Explorer root={'downloads'}/></Tab.Pane>,
+      render: () => <Tab.Pane><Explorer root={'downloads'} isActive={tab === 'incomplete'}/></Tab.Pane>,
     },
     { 
       route: 'incomplete',
       menuItem: 'Incomplete',
-      render: () => <Tab.Pane><Explorer root={'incomplete'}/></Tab.Pane>,
+      render: () => <Tab.Pane><Explorer root={'incomplete'} isActive={tab === 'incomplete'}/></Tab.Pane>,
     },
   ];
+
+  const onTabChange = (e, { activeIndex }) => {
+    setTab(panes[activeIndex].route);
+  };
 
   return (
     <div>
       <Tab
         panes={panes}
+        onTabChange={onTabChange}
       />
     </div>
   );
