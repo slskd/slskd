29ee68346f6649fe4a3865645d565f4752c2a92d
JP Dillingham <jp@dillingham.me>
2023-07-23 17:46:44 -0500

    remove tab tracking

--- src/web/src/components/System/Files/index.js
+++ src/web/src/components/System/Files/index.js
@@ -1,34 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Tab } from 'semantic-ui-react';
 
 import './Files.css';
 import Explorer from './Explorer';
 
 const Files = () => {
-  const [tab, setTab] = useState('downloads');
-
   const panes = [
     { 
       route: 'downloads',
       menuItem: 'Downloads',
-      render: () => <Tab.Pane><Explorer root={'downloads'} isActive={tab === 'incomplete'}/></Tab.Pane>,
+      render: () => <Tab.Pane><Explorer root={'downloads'}/></Tab.Pane>,
     },
     { 
       route: 'incomplete',
       menuItem: 'Incomplete',
-      render: () => <Tab.Pane><Explorer root={'incomplete'} isActive={tab === 'incomplete'}/></Tab.Pane>,
+      render: () => <Tab.Pane><Explorer root={'incomplete'}/></Tab.Pane>,
     },
   ];
-
-  const onTabChange = (e, { activeIndex }) => {
-    setTab(panes[activeIndex].route);
-  };
 
   return (
     <div>
       <Tab
         panes={panes}
-        onTabChange={onTabChange}
       />
     </div>
   );
