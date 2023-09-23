bc9373054f1d1ce75f65961890021cf8547da66b
JP Dillingham <jp@dillingham.me>
2023-09-23 13:28:00 -0500

    add file retention options

--- src/slskd/Core/Options.cs
+++ src/slskd/Core/Options.cs
@@ -1060,6 +1060,12 @@ namespace slskd
         public class RetentionOptions
         {
             /// <summary>
+            ///     Gets file retention options.
+            /// </summary>
+            [Validate]
+            public FileRetentionOptions File { get; init; } = new FileRetentionOptions();
+
+            /// <summary>
             ///     Gets upload retention options.
             /// </summary>
             [Validate]
@@ -1070,6 +1076,22 @@ namespace slskd
             /// </summary>
             [Validate]
             public TransferRetentionOptions Download { get; init; } = new TransferRetentionOptions();
+
+            /// <summary>
+            ///     File retention options.
+            /// </summary>
+            public class FileRetentionOptions
+            {
+                /// <summary>
+                ///     Gets the time to retain completed files, in minutes.
+                /// </summary>
+                public int? Complete { get; init; } = null;
+
+                /// <summary>
+                ///     Gets the time to retain incomplete files, in minutes.
+                /// </summary>
+                public int? Incomplete { get; init; } = null;
+            }
 
             /// <summary>
             ///     Transfer retention options.
