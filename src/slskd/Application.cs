14cdb89836575741104cfcf73f446233f18c3809
JP Dillingham <jp@dillingham.me>
2023-10-01 22:16:53 -0500

    implement file pruning

--- src/slskd/Application.cs
+++ src/slskd/Application.cs
@@ -744,18 +744,52 @@ namespace slskd
 
         private void Clock_EveryHour(object sender, EventArgs e)
         {
-            _ = Task.Run(() => PruneLogs());
         }
 
         private void PruneFiles()
         {
-            // todo: incomplete
-            // todo: complete
-        }
+            void PruneDirectory(int? age, string directory)
+            {
+                if (!age.HasValue)
+                {
+                    return;
+                }
 
-        private void PruneLogs()
-        {
-            // todo: delete files matching pattern slskd-20230216.log
+                Log.Debug("Pruning files older than {Age} minutes from {Directory}", age, directory);
+
+                var options = new EnumerationOptions
+                {
+                    IgnoreInaccessible = true,
+                    AttributesToSkip = FileAttributes.Hidden | FileAttributes.System,
+                    RecurseSubdirectories = true,
+                };
+
+                var files = System.IO.Directory.GetFiles(directory, "*", options)
+                    .Select(filename => new FileInfo(filename))
+                    .Where(file => file.LastAccessTimeUtc <= DateTime.UtcNow.AddMinutes(-age.Value));
+
+                Log.Debug("Found {Count} files of need of pruning", files.Count());
+
+                int errors = 0;
+
+                foreach (var file in files)
+                {
+                    try
+                    {
+                        file.Delete();
+                    }
+                    catch (Exception ex)
+                    {
+                        errors++;
+                        Log.Warning(ex, "Failed to prune file {File}: {Message}", file, ex.Message);
+                    }
+                }
+
+                Log.Debug("Pruning complete. Deleted: {Deleted}, Errors: {Errors}", files.Count() - errors, errors);
+            }
+
+            PruneDirectory(age: Options.Retention.File.Incomplete, directory: Options.Directories.Incomplete);
+            PruneDirectory(age: Options.Retention.File.Complete, directory: Options.Directories.Downloads);
         }
 
         private void PruneTransfers()
