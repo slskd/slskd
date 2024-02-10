25f595414ea51388aacca7269f9949ffe6db1151
Joshua May <git@hotdog.horse>
2024-02-11 00:29:44 +0100

    Add jsx parsing

--- src/web/.eslintrc.js
+++ src/web/.eslintrc.js
@@ -1,11 +1,25 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ['canonical/auto', 'canonical/browser', 'canonical/node'],
-  ignorePatterns: ['build'],
+  ignorePatterns: ['build', 'node_modules', 'package-lock.json'],
   overrides: [
     {
-      extends: ['canonical/jsx-a11y'],
-      files: '*.jsx',
+      extends: [
+        'canonical',
+        'canonical/regexp',
+        'canonical/jsdoc',
+        'canonical/jsx-a11y',
+        'canonical/react',
+        'canonical/prettier',
+      ],
+      files: ['*.jsx'],
+      parserOptions: {
+        babelOptions: {
+          parserOpts: {
+            plugins: ['jsx'],
+          },
+        },
+      },
     },
     {
       extends: ['canonical/jest'],
