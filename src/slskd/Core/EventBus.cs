65e3399b3361bb682a03bb4d8b52f3baf390a4ce
JP Dillingham <jp@dillingham.me>
2024-08-12 21:02:41 -0500

    tweak logs

--- src/slskd/Core/EventBus.cs
+++ src/slskd/Core/EventBus.cs
@@ -72,7 +72,7 @@ public class EventBus
             return;
         }
 
-        Log.Debug("{Count} subscribers for {Type}", subscribers.Count, typeof(T));
+        Log.Debug("{Count} subscriber(s) for {Type}: {Names}", subscribers.Count, typeof(T), string.Join(", ", subscribers.Keys));
 
         // we don't care about any of these tasks; contractually we are only obligated to invoke them
         _ = Task.WhenAll(subscribers.Select(subscriber =>
