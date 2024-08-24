da7371484fcf256eb1efe394593cb6d748043afc
JP Dillingham <jp@dillingham.me>
2024-08-24 17:46:08 -0500

    add dbcontext for events

---
+++ src/slskd/Events/EventsDbContext.cs
@@ -0,0 +1,41 @@
+// <copyright file="EventsDbContext.cs" company="slskd Team">
+//     Copyright (c) slskd Team. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU Affero General Public License as published
+//     by the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU Affero General Public License for more details.
+//
+//     You should have received a copy of the GNU Affero General Public License
+//     along with this program.  If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace slskd.Events
+{
+    public class EventsDbContext : DbContext
+    {
+        public EventsDbContext(DbContextOptions<EventsDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<EventRecord> Events { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<EventRecord>()
+                .Property(e => e.Timestamp)
+                .HasConversion(v => v, v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+        }
+    }
+}
