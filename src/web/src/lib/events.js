e5659ce50fe1344f5ccf7229482ba450193e389a
JP Dillingham <jp@dillingham.me>
2024-10-06 16:56:38 -0500

    retrieve total count from header

--- src/web/src/lib/events.js
+++ src/web/src/lib/events.js
@@ -1,7 +1,10 @@
 import api from './api';
 
 export const list = async ({ offset, limit }) => {
-  const response = (await api.get(`/events?offset=${offset}&limit=${limit}`))
-    .data;
-  return response;
+  const response = await api.get(`/events?offset=${offset}&limit=${limit}`);
+
+  const events = response.data;
+  const totalCount = response.headers['x-total-count'];
+
+  return { events, totalCount };
 };
