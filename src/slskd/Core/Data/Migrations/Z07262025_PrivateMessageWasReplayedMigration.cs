871999808e44c60a6939aa2361204a7be03f0cd1
JP Dillingham <jp@dillingham.me>
2025-07-27 13:50:26 -0500

    add migration for WasReplayed

---
+++ src/slskd/Core/Data/Migrations/Z07262025_PrivateMessageWasReplayedMigration.cs
@@ -0,0 +1,81 @@
+// <copyright file="Z07262025_PrivateMessageWasReplayedMigration.cs" company="slskd Team">
+//     Copyright (c) slskd Team. All rights reserved.
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU Affero General Public License as published
+//     by the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU Affero General Public License for more details.
+//
+//     You should have received a copy of the GNU Affero General Public License
+//     along with this program.  If not, see https://www.gnu.org/licenses/.
+// </copyright>
+
+namespace slskd.Migrations;
+
+using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Data.Sqlite;
+using Serilog;
+using slskd.Messaging;
+
+/// <summary>
+///     Updates the Transfers table to add indexes on the Direction and State columns.
+/// </summary>
+public class Z07262025_PrivateMessageWasReplayedMigration : IMigration
+{
+    private ILogger Log { get; } = Serilog.Log.ForContext<Z07262025_PrivateMessageWasReplayedMigration>();
+    private string DatabaseName { get; } = "messaging";
+    private string ConnectionString => $"Data Source={Path.Combine(Program.DataDirectory, $"{DatabaseName}.db")}";
+
+    public bool NeedsToBeApplied()
+    {
+        // check to see if *BOTH* of the indexes are in place. if one or both are missing, we must apply
+        var schema = SchemaInspector.GetDatabaseSchema(ConnectionString);
+        var pms = schema["PrivateMessages"];
+
+        if (pms.Any(r => r.Name == nameof(PrivateMessage.WasReplayed)))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public void Apply()
+    {
+        if (!NeedsToBeApplied())
+        {
+            Log.Information("> Migration {Name} is not necessary or has already been applied", nameof(Z07262025_PrivateMessageWasReplayedMigration));
+            return;
+        }
+
+        using var connection = new SqliteConnection(ConnectionString);
+        connection.Open();
+
+        using var transaction = connection.BeginTransaction();
+
+        try
+        {
+            Log.Information("> Adding missing column WasReplayed to PrivateMessages table...");
+
+            var addColumnCommand = new SqliteCommand(@"
+                ALTER TABLE PrivateMessages ADD COLUMN WasReplayed INTEGER NOT NULL DEFAULT 0
+            ", connection, transaction);
+            addColumnCommand.ExecuteNonQuery();
+
+            transaction.Commit();
+            Log.Information("> Done!");
+        }
+        catch (Exception)
+        {
+            transaction.Rollback();
+            throw;
+        }
+    }
+}
