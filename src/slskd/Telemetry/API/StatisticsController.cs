2a1d9b6eaafe0d61f1a27930b7ab4fb041faa92b
JP Dillingham <jpdillingham@gmail.com>
2025-08-31 23:27:37 -0500

    split telemetry controllers

--- src/slskd/Telemetry/API/StatisticsController.cs
+++ src/slskd/Telemetry/API/StatisticsController.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="TelemetryController.cs" company="slskd Team">
+﻿// <copyright file="StatisticsController.cs" company="slskd Team">
 //     Copyright (c) slskd Team. All rights reserved.
 //
 //     This program is free software: you can redistribute it and/or modify
@@ -20,7 +20,6 @@ namespace slskd.Telemetry;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Asp.Versioning;
 using Microsoft.AspNetCore.Authorization;
@@ -30,78 +29,22 @@ using Soulseek;
 /// <summary>
 ///     Telemetry.
 /// </summary>
-[Route("api/v{version:apiVersion}/[controller]")]
+[Route("api/v{version:apiVersion}/telemetry/[controller]")]
 [ApiVersion("0")]
 [ApiController]
 [Produces("application/json")]
-public class TelemetryController : ControllerBase
+public class StatisticsController : ControllerBase
 {
     /// <summary>
-    ///     Initializes a new instance of the <see cref="TelemetryController"/> class.
+    ///     Initializes a new instance of the <see cref="StatisticsController"/> class.
     /// </summary>
     /// <param name="telemetryService"></param>
-    public TelemetryController(TelemetryService telemetryService)
+    public StatisticsController(TelemetryService telemetryService)
     {
         Telemetry = telemetryService;
     }
 
     private TelemetryService Telemetry { get; }
-
-    /// <summary>
-    ///     KPIs for the app, based on the Prometheus dashboard
-    ///     * process_Working_set_bytes = total memory used
-    ///     * dotnet_total_memory_bytes = sum of all managed memory
-    ///     * process_cpu_seconds_total = total cpu time spent (not a great measure)
-    ///     * system_runtime_cpu_usage = % of cpu consumed
-    ///     * system_net_sockets_* = TCP activity
-    ///     KPIs for the _system_, not the app
-    ///     * node_memory_* = info about _system_ memory
-    ///     * node_filesystem_avail_bytes = how much space is left on the system
-    ///     * node_network = info about network adapters on the system
-    ///     Other
-    ///     * process_start_time_seconds = unix timestamp of start time
-    /// </summary>
-    private List<Regex> KpiRegexes { get; } = new List<Regex>
-    {
-        new Regex("slskd_.*", RegexOptions.Compiled),
-        new Regex("node_(?!cpu)", RegexOptions.Compiled),
-        new Regex("process_.*", RegexOptions.Compiled),
-        new Regex("dotnet_total_memory_bytes", RegexOptions.Compiled),
-        new Regex("system_runtime_[cpu_usage|working_set|alloc_total]", RegexOptions.Compiled),
-        new Regex("system_net_sockets.*", RegexOptions.Compiled),
-        new Regex("microsoft_aspnetcore_server_kestrel_[current|total]_connections", RegexOptions.Compiled),
-    };
-
-    /// <summary>
-    ///     Gets application metrics.
-    /// </summary>
-    /// <returns></returns>
-    [HttpGet("metrics")]
-    [Authorize(Policy = AuthPolicy.Any)]
-    [Produces("text/plain", "application/json")]
-    public async Task<IActionResult> Get()
-    {
-        if (Request.Headers.Accept.ToString().Equals("application/json", StringComparison.OrdinalIgnoreCase))
-        {
-            Dictionary<string, PrometheusMetric> dict = await Telemetry.Prometheus.GetMetricsAsObject();
-            return Ok(dict);
-        }
-
-        var response = await Telemetry.Prometheus.GetMetricsAsString();
-        return Content(response, "text/plain");
-    }
-
-    /// <summary>
-    ///     Gets gets key performance indicators for the application.
-    /// </summary>
-    /// <returns></returns>
-    [HttpGet("metrics/kpis")]
-    [Authorize(Policy = AuthPolicy.Any)]
-    public async Task<IActionResult> GetKpis()
-    {
-        Dictionary<string, PrometheusMetric> dict = await Telemetry.Prometheus.GetMetricsAsObject(include: KpiRegexes);
-        return Ok(dict);
-    }
 
     /// <summary>
     ///     Summarizes transfer statistics for all users for the specified time range.
