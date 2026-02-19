570cfb6eb96488f65a2d8761dc74e870e07098a5
JP Dillingham <jp@dillingham.me>
2026-02-18 18:55:11 -0600

    map groupid and attempts when copying

--- src/slskd/Transfers/Extensions.cs
+++ src/slskd/Transfers/Extensions.cs
@@ -23,6 +23,7 @@ namespace slskd.Transfers
         {
             return new Transfer()
             {
+                GroupId = transfer.GroupId,
                 Id = transfer.Id,
                 Username = transfer.Username,
                 Direction = transfer.Direction,
@@ -37,6 +38,7 @@ namespace slskd.Transfers
                 BytesTransferred = t.BytesTransferred,
                 AverageSpeed = t.AverageSpeed,
                 Exception = t.Exception?.Message,
+                Attempts = transfer.Attempts,
             };
         }
     }
