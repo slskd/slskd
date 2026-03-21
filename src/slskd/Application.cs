cb964333df266a7908b79376ae57c9c085107498
JP Dillingham <jp@dillingham.me>
2026-03-21 15:48:15 -0500

    update header

--- src/slskd/Application.cs
+++ src/slskd/Application.cs
@@ -1,10 +1,15 @@
-﻿// <copyright file="Application.cs" company="slskd Team">
-//     Copyright (c) slskd Team. All rights reserved.
+﻿// <copyright file="Application.cs" company="JP Dillingham">
+//
+//           ▄▄▄▄     ▄▄▄▄     ▄▄▄▄
+//     ▄▄▄▄▄▄█  █▄▄▄▄▄█  █▄▄▄▄▄█  █
+//     █__ --█  █__ --█    ◄█  -  █
+//     █▄▄▄▄▄█▄▄█▄▄▄▄▄█▄▄█▄▄█▄▄▄▄▄█
+//
+//     Copyright (c) JP Dillingham and contributors.
 //
 //     This program is free software: you can redistribute it and/or modify
 //     it under the terms of the GNU Affero General Public License as published
-//     by the Free Software Foundation, either version 3 of the License, or
-//     (at your option) any later version.
+//     by the Free Software Foundation, version 3.
 //
 //     This program is distributed in the hope that it will be useful,
 //     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,6 +18,15 @@
 //
 //     You should have received a copy of the GNU Affero General Public License
 //     along with this program.  If not, see https://www.gnu.org/licenses/.
+//
+//     This program is distributed with Additional Terms pursuant to
+//     Section 7 of the GNU Affero General Public License.  See the
+//     LICENSE file in the root directory of this project for the
+//     complete terms and conditions.
+//
+//     SPDX-FileCopyrightText: JP Dillingham and contributors
+//     SPDX-License-Identifier: AGPL-3.0-only
+//
 // </copyright>
 
 using Microsoft.Extensions.Options;
