3c799f205ce56e250883efd1deda798b758bb788
JP Dillingham <jp@dillingham.me>
2026-05-17 17:39:43 -0500

    add test for greedy bracket change

---
+++ tests/slskd.Tests.Unit/Core/Options/SharesOptionsTests.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
+using static slskd.Options;
+
+namespace slskd.Tests.Unit.Core.Options;
+
+public class SharesOptionsTests
+{
+    private static List<ValidationResult> Validate(SharesOptions options)
+    {
+        var context = new ValidationContext(options);
+        var results = new List<ValidationResult>();
+        Validator.TryValidateObject(options, context, results, validateAllProperties: true);
+        return results;
+    }
+
+    public class Directories
+    {
+        public class Digest
+        {
+            [Fact]
+            public void Alias_Uses_First_Closing_Bracket_When_Path_Contains_Brackets()
+            {
+                // [foo]/bar/[baz]/ should digest to alias "foo" and path "/bar/[baz]"
+                var options = new SharesOptions
+                {
+                    Directories = [@"[foo]/bar/[baz]/"]
+                };
+
+                var results = Validate(options);
+
+                Assert.DoesNotContain(results, r => r.ErrorMessage?.Contains("aliases may not contain path separators") == true);
+            }
+        }
+    }
+}
